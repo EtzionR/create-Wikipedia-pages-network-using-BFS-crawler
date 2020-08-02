be3cd558bf067f02c0bc8e3f96794fe4eae8b8c0
Etzion Harari <etzionhar@gmail.com>
2020-08-02 03:25:42 +0300

    Update implementation.py

--- implementation.py
+++ implementation.py
@@ -2,7 +2,7 @@ from wiki_crawler import wikipedia_network
 
 url = r'https://en.wikipedia.org/wiki/George_Washington'
 wikipedia_network(url, depth = 1,plot = True)
-v= input('tt')
+
 url = r'https://en.wikipedia.org/wiki/Harry_Potter_(character)'
 wikipedia_network(url, depth = 2,plot = True)
 
